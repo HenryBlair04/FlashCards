be9d80dbc1f8ffd235b6cb8ff3ebe3b25644e65e
iD Student <idstudent@stratton-three-forty-five.mit.edu>
2017-07-19 13:38:36 -0400

    I put the questions, question numbers, answers/options in it and are working fine. "Final Answer" button has not been completed yet so you cannot move between questions.

---
+++ flashcards/FlashCards.swift
@@ -0,0 +1,72 @@
+//
+//  FlashCards.swift
+//  flashcards
+//
+//  Created by iD Student on 7/18/17.
+//  Copyright © 2017 iD Blair. All rights reserved.
+//
+
+import Foundation
+
+class Flashcard {
+    var question : String
+    
+    var options : [String]
+    var correctAnswer : String
+    
+    init (_ question1 : String, _ options1 : [String] ) {
+        self.question = question1
+        self.options = options1
+        
+        self.correctAnswer = options [1]
+    }
+}
+
+
+class CardCollection {
+    
+    //single instance of the card collection. card collectio represemnts the same cards that were using over and over again. If i want to access it i will use CardCollection.instance and if i add an extention like .currentIndex then i can access that too//
+    public static var instance : CardCollection = CardCollection ()
+    
+    public private (set) var cards : [Flashcard]     //this means anyone can access but only I can set the cards.. same with next few lines//
+    
+    public private (set) var currentIndex : Int
+    
+    public var currentCard : Flashcard {
+        get { return cards [ currentIndex ] }  // this means that class CardCollection can return a flashcard from the array of "cards" (see below) based on the current index which determines which card to return. A current index of 0 will give you the first card and 4 would be the last
+    }
+    
+    private init () {
+        //look up at the class flashcard. Notice before i had it like Flashcard (question1:"__________"), options: ["___" "___"] but now because i added the underscore it assumes the question1 and options1 part of the flashcard and i don't have to type that. I will leave one old fashioned one with those extra writings as a comment for rederence.//
+        
+        cards = [ //array of cards//
+            Flashcard("What day is it?", ["Monday", "Tuesday"]),
+            Flashcard ("How are you", ["good", "bad"]),
+            Flashcard ("What's the capital of the Washington State?", ["Olympia", "Tacoma", "seattle", "Spokane"]),
+            Flashcard ("Where is iD Tech's headquarters?",["Campbell, CA", "Alberque, NM", "Beijing, CN" ]),
+            Flashcard ("How old ar eyou", ["16", "15", "14", "13"]),
+           // Flashcard (question1: "How old ar ey", options1: ["16", "15", "14", "13"])//
+        ]
+    
+        currentIndex = 0;
+    }
+        
+    public func nextQuestion () {
+        currentIndex += 1    //this increases the currentIndex by 1 so you can move forward //
+        if (currentIndex >= cards.count) {
+            currentIndex = 0
+        }
+    }
+    
+    public func checkAnswer (_ selectedAnswer : Int) -> Bool {
+        return currentCard.options [ selectedAnswer ] == currentCard.correctAnswer
+    }
+    
+    
+
+}
+
+//now we are going to do the view controller. this is takes informaiton and displays it from the CardCollection class//
+
+
+
--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+//expand and add to view controller//
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var questionTextView: UITextView!
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    @IBOutlet weak var questionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        answerPickerView.dataSource = self;
+        answerPickerView.delegate = self;
+        
+        setupCardUI ()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func setupCardUI () { //this updates question number, question text and answers/options //
+        answerPickerView.reloadAllComponents ()
+        questionTextView.text = CardCollection.instance.currentCard.question
+        //what this function does is displays the current question in the question text view which we just put on this viewcontroller. This allows us to connect the controller to the view.The CardCollection class is connected with instance as i explained in the swift main thing and this allows it to display the current card and its question in the question text view//
+        questionLabel.text = "Question \(CardCollection.instance.currentIndex + 1) / \(CardCollection.instance.cards.count)"
+    }
+    
+    public func numberOfComponents(in pickerView:UIPickerView) -> Int {
+        return 1;
+        //one column to chose from so returning one
+        
+    }
 
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
 
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row];
+    }
 }
+
+
 
